18094860279b7140f5c7d4151a45ded067e7d6cf
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-12-15 20:38:58 +0530

    Router done

--- src/Components/Inbox.jsx
+++ src/Components/Inbox.jsx
@@ -1,42 +1,50 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const dummy=[
-    {
-        id:1,sender:"tm17@gmail.com",subject:"Helo here is your material for studies thank you"
-    },
-    {
-        id:2,sender:"tm17@gmail.com",subject:"Helo here is your material for studies thank you"
-    },
-    {
-        id:3,sender:"tm17@gmail.com",subject:"Helo here is your material for studies thank you"
-    },
-    {
-        id:4,sender:"tm17@gmail.com",subject:"Helo here is your material for studies thank you"
-    },
-    {
-        id:5,sender:"tm17@gmail.com",subject:"Helo here is your material for studies thank you"
-    }
-]
+const dummy = [
+  {
+    id: 1,
+    sender: "tm17@gmail.com",
+    subject: "Helo here is your material for studies thank you",
+  },
+  {
+    id: 2,
+    sender: "tm17@gmail.com",
+    subject: "Helo here is your material for studies thank you",
+  },
+  {
+    id: 3,
+    sender: "tm17@gmail.com",
+    subject: "Helo here is your material for studies thank you",
+  },
+  {
+    id: 4,
+    sender: "tm17@gmail.com",
+    subject: "Helo here is your material for studies thank you",
+  },
+  {
+    id: 5,
+    sender: "tm17@gmail.com",
+    subject: "Helo here is your material for studies thank you",
+  },
+];
 function Inbox() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="bg-white shadow-lg rounded-md">
-        <h1>Inbox</h1>
+    <div className="w-[100%] flex items-center justify-center">
+     
+      <div className="bg-white p-5 shadow-lg rounded-md">
+      <h1 className="text-2xl font-bold">Inbox</h1>
         <ul>
-            {dummy.map((email)=> (
-                <li className="border-b py-2" key={email.id}>
-                    <Link to={`/mail/${email.id}`}>
-                    <strong>{email.sender}</strong>:{email.subject}
-                    </Link>
-                    
-                  
-                </li>
-            ))}
+          {dummy.map((email) => (
+            <li className="border-b py-2 m-5  hover:bg-slate-300 p-5 rounded-full" key={email.id}>
+              <Link to={`/mail/${email.id}`}>
+                <strong>{email.sender}</strong>:{email.subject}
+              </Link>
+            </li>
+          ))}
         </ul>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default Inbox
+export default Inbox;
